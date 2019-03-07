64a5b804350d5a257d36dcd57c3c33c584717c9d
Sascha Metz <metz@raum3.de>
2019-03-07 15:18:32 +0100

    Finishing Rating Component

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,59 @@
-export default () => <h2>Bandvote</h2>;
+import TableStyles from '../components/TableStyles';
+
+class Index extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      bands: []
+    };
+  }
+
+  componentDidMount() {
+    const bandsFromStorage = localStorage.getItem('suobands');
+    if (bandsFromStorage)
+      this.setState({ bands: JSON.parse(bandsFromStorage) });
+  }
+
+  render() {
+    const { bands } = this.state;
+    return (
+      <div className="container">
+        <h2>Alle Bands ({bands.length})</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Genre</th>
+              <th>Links</th>
+              <th>Gesamteindruck</th>
+              <th>Sänger</th>
+              <th>Qualität</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bands.map((band, index) => (
+              <tr key={index}>
+                <td>{band.name}</td>
+                <td>{band.genre}</td>
+                <td>
+                  {band.links.map(link => (
+                    <>
+                      {link}
+                      <br />
+                    </>
+                  ))}
+                </td>
+                <td>{band.rating.overall}</td>
+                <td>{band.rating.singer}</td>
+                <td>{band.rating.quality}</td>
+              </tr>
+            ))}
+          </tbody>
+          <TableStyles />
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Index;

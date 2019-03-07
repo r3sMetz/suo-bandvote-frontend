a4b133ec85b310a062839c55f93e7343fb89e984
Sascha Metz <metz@raum3.de>
2019-03-07 19:24:33 +0100

    Building ResultTable.js component

---
+++ components/ResultTable.js
@@ -0,0 +1,50 @@
+import PropTypes from 'prop-types';
+import TableStyles from './TableStyles';
+
+class ResultTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bands: props.bands
+    };
+  }
+
+  render() {
+    const bands = this.state.bands;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Platz</th>
+            <th>Name</th>
+            <th>Punkte</th>
+          </tr>
+        </thead>
+        <tbody>
+          {bands.map((band, index) => {
+            const { overall, singer, quality } = band.rating;
+            return (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{band.name}</td>
+                <td>{overall + singer + quality}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+        <TableStyles />
+      </table>
+    );
+  }
+}
+
+// PropTypes + Default Props
+ResultTable.defaultProps = {
+  bands: []
+};
+
+ResultTable.propTypes = {
+  bands: PropTypes.array.isRequired
+};
+
+export default ResultTable;
